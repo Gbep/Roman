292f429994fb31d07f38b88b17513b87addbbc12
Roman <122663994+Gbep@users.noreply.github.com>
2023-04-24 19:47:39 +0300

    Add files via upload

---
+++ Console.Hnep/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File does not exist.");
+            Console.WriteLine("File does not exist.");
+            Console.WriteLine("File has been written.");
+            Console.ReadKey();
+        }
+    }
+
+    public interface IRead
+    {
+        string Read(string filePath);
+    }
+
+    public interface IWrite
+    {
+        void Write(string filePath, string content);
+    }
+
+
+    public class FileService : IRead, IWrite
+    {
+        public string Read(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist.");
+                return null!;
+            }
+            string text = File.ReadAllText(filePath);
+            Console.WriteLine(text);
+            return text;
+        }
+
+        public void Write(string filePath, string content)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist.");
+                return;
+            }
+            File.WriteAllText(filePath, content);
+            Console.WriteLine("File has been written.");
+        }
+    }
+
+
